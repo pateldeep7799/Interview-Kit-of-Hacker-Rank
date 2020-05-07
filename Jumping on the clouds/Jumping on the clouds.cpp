fd5f9e1450e1befa330fc192b4914ac239b22c51
Patel Deep <63906505+pateldeep7799@users.noreply.github.com>
2020-05-07 17:03:37 +0530

    Add files via upload

---
+++ Jumping on the clouds/Jumping on the clouds.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+int main() {
+
+ int N,ar[102],cnt = 0;
+ cin >> N;
+ for(int n = 0; n < N; n++)cin >> ar[n];
+ for(int i = 1; i < N;cnt++){
+        if(ar[i+1] != 1 && i < N-1)i+=2;
+        else i++;  
+ }
+ cout << cnt << endl;
+ return 0;
+}
